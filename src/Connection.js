c913cd0d4782972a0d4fa4e12824700e08ebdaa3
LordChunk <LordChunk@users.noreply.github.com>
2024-12-13 13:32:32 +0100

    Removed discordjs/opus dependency

--- src/Connection.js
+++ src/Connection.js
@@ -6,8 +6,8 @@ const tls = require('tls');
 const Protobuf = require('./Protobuf')
 const Promise = require('bluebird')
 const EventEmitter = require('events').EventEmitter
-const OpusEncoder = require('@discordjs/opus').OpusEncoder
-const Constants = require('./Constants')
+// const OpusEncoder = require('@discordjs/opus').OpusEncoder
+// const Constants = require('./Constants')
 const Util = require('./Util')
 
 class Connection extends EventEmitter {
@@ -15,8 +15,8 @@ class Connection extends EventEmitter {
         super()
 
         this.options = options;
-        this.opusEncoder = new OpusEncoder(Constants.Audio.sampleRate, 1)
-        this.currentEncoder = this.opusEncoder
+        // this.opusEncoder = new OpusEncoder(Constants.Audio.sampleRate, 1)
+        // this.currentEncoder = this.opusEncoder
         this.codec = Connection.codec().Opus
         this.voiceSequence = 0
         this.codecWarningShown = {};
@@ -165,59 +165,60 @@ class Connection extends EventEmitter {
             sequence,
             lastFrame,                   // Don't rely on it!
             opusData,                    // Voice data encoded, as it came in
-            decodedData: () => this.currentEncoder.decode(opusData) // Voice data decoded (48000, 1ch, 16bit)
+            // decodedData: () => this.currentEncoder.decode(opusData) // Voice data decoded (48000, 1ch, 16bit)
         }
 
         this.emit('voiceData', voiceData);
     }
 
     writeAudio(packet, whisperTarget, codec, voiceSequence, final) {
-        packet = this.currentEncoder.encode(packet)
+        console.error('writeAudio not implemented');
+        // packet = this.currentEncoder.encode(packet)
 
-        const type = codec === Connection.codec().Opus ? 4 : 0
-        const target = whisperTarget || 0
-        const typeTarget = type << 5 | target
+        // const type = codec === Connection.codec().Opus ? 4 : 0
+        // const target = whisperTarget || 0
+        // const typeTarget = type << 5 | target
 
-        if (typeof voiceSequence !== 'number')
-            voiceSequence = this.voiceSequence
+        // if (typeof voiceSequence !== 'number')
+        //     voiceSequence = this.voiceSequence
 
-        const sequenceVarint = Util.toVarint(voiceSequence)
+        // const sequenceVarint = Util.toVarint(voiceSequence)
 
-        const voiceHeader = Buffer.alloc(1 + sequenceVarint.length)
-        voiceHeader[0] = typeTarget
-        sequenceVarint.value.copy(voiceHeader, 1, 0)
-        let header
+        // const voiceHeader = Buffer.alloc(1 + sequenceVarint.length)
+        // voiceHeader[0] = typeTarget
+        // sequenceVarint.value.copy(voiceHeader, 1, 0)
+        // let header
 
-        if (codec == Connection.codec().Opus) {
-            if (packet.length > 0x1FFF)
-                throw new TypeError(`Audio frame too long! Max Opus length is ${0x1FFF} bytes.`)
+        // if (codec == Connection.codec().Opus) {
+        //     if (packet.length > 0x1FFF)
+        //         throw new TypeError(`Audio frame too long! Max Opus length is ${0x1FFF} bytes.`)
 
-            let headerValue = packet.length
+        //     let headerValue = packet.length
 
-            if (final)
-                headerValue += (1 << 7)
+        //     if (final)
+        //         headerValue += (1 << 7)
 
-            const headerVarint = Util.toVarint(headerValue)
-            header = headerVarint.value
-        } else {
-            throw new TypeError('Celt is not supported')
-        }
+        //     const headerVarint = Util.toVarint(headerValue)
+        //     header = headerVarint.value
+        // } else {
+        //     throw new TypeError('Celt is not supported')
+        // }
 
-        const frame = Buffer.alloc(header.length + packet.length)
-        header.copy(frame, 0)
+        // const frame = Buffer.alloc(header.length + packet.length)
+        // header.copy(frame, 0)
 
-        packet.copy(frame, header.length)
+        // packet.copy(frame, header.length)
 
-        voiceSequence++
+        // voiceSequence++
 
-        this._writeHeader(this.protobuf.idByName('UDPTunnel'), voiceHeader.length + frame.length)
-        this._writePacket(voiceHeader)
-        this._writePacket(frame)
+        // this._writeHeader(this.protobuf.idByName('UDPTunnel'), voiceHeader.length + frame.length)
+        // this._writePacket(voiceHeader)
+        // this._writePacket(frame)
 
-        if (voiceSequence > this.voiceSequence)
-            this.voiceSequence = voiceSequence
+        // if (voiceSequence > this.voiceSequence)
+        //     this.voiceSequence = voiceSequence
 
-        return 1
+        // return 1
     }
 }
 
